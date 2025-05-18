423226375b9d95c963cc10664440742a6f964983
rahul122703 <killbusyness@gmail.com>
2025-05-19 03:05:15 +0530

    fixed size of product page

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -41,10 +41,10 @@ export default function ProductPage() {
   }
   if (singleproductLoading || !singleproduct?.fields) {
     return (
-      <div className="min-h-screen bg-gray-50 p-4">
+      <>
         <ProductPageSkeleton />
         <Footer />
-      </div>
+      </>
     );
   }
 
@@ -64,7 +64,7 @@ export default function ProductPage() {
   const imageUrl = image?.[0]?.url || "";
 
   return (
-    <div className="bg-gray-50 min-h-screen p-6">
+    <>
       <div className="max-w-[100rem] mx-auto bg-white rounded-3xl shadow-2xl p-6 border border-gray-200">
         <button
           className="mb-6 px-4 py-2 bg-gray-700 hover:bg-black text-white rounded-lg shadow flex items-center gap-2"
@@ -127,6 +127,6 @@ export default function ProductPage() {
         </div>
       </div>
       <Footer />
-    </div>
+    </>
   );
 }
