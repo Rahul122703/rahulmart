a812fa309dd3ed0f0986f422feb7dbbd493c8530
rahul122703 <killbusyness@gmail.com>
2025-06-09 17:04:45 +0530

    some minor changes

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -58,7 +58,6 @@ export const FilterProvider = ({ children }) => {
     const name = e.target.name;
     if (name === "free_shipping") {
       value = e.target.checked;
-      console.log(value);
     }
     dispatch({ type: UPDATE_FILTERS, payload: { value, name } });
   };
@@ -78,23 +77,3 @@ export const FilterProvider = ({ children }) => {
 export const useFilterContext = () => {
   return useContext(filterContext);
 };
-
-// {
-//     "id": "rec0sKqV6bzMrtkcR",
-//     "image": "https://v5.airtableusercontent.com/v3/u/41/41/1747951200000/gJILmg0I6Mt-j27AFDeGjg/DdDxiMBiCadUAoXqedanlmfDET5XnEyyWSaellYVfb3S6tpjnp0gyg5hsfdbza8uLJYSYv15E_I0oyPfiwK07KeDiAuAZBiO34XedokT8DaodDkCG_7UdLbOcXc0IMQJo1iAUDV1T87BkIGq_S1c4YJXUq2AbXO3jn86fbHsKm4/Qva6yN0Rhi8EYnPvT7UXM6A46kyDPkOtbPZFKGG0H1E",
-//     "price": 1999,
-//     "name": "Hue White LED Bulb",
-//     "stock": 4,
-//     "rating": 2,
-//     "colors": [
-//       "blue",
-//       "black",
-//       "purple",
-//       "grey"
-//     ],
-//     "company": "Philips",
-//     "desc": "This is a sports product made by Philips. It comes in blue, black, purple, grey colors and is designed for quality and durability. Suitable for all your daily needs, this product ensures both comfort and performance. A perfect choice for anyone looking for value and functionality.",
-//     "category": "Home Improvement",
-//     "featured": "false",
-//     "shipping": "false"
-//   }
